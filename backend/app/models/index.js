913b7b2c481b902cbbfe122fa5f077f7bd2b7d1c
nshiva20 <112461911+nshiva20@users.noreply.github.com>
2022-09-15 18:12:09 +0530

    UI commit with API call

--- backend/app/models/index.js
+++
@@ -1,9 +0,0 @@
-// Requires official Node.js MongoDB Driver 3.0.0+
-const dbConfig = require("../config/db.config.js");
-const mongoose = require("mongoose");
-mongoose.Promise = global.Promise;
-const db = {};
-db.mongoose = mongoose;
-db.url = dbConfig.url;
-db.userData = require("./userData.model.js")(mongoose);
-module.exports = db;
